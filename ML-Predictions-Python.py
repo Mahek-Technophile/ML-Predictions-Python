38f9de5333f54d8cb910d335e93f1bae7127ab8a
Mahek Shukla <mahekrutuja@gmail.com>
2025-01-09 22:26:20 +0530

    Update ML-Predictions-Python.py

--- ML-Predictions-Python.py
+++ ML-Predictions-Python.py
@@ -1,5 +1,5 @@
 !pip install pandas numpy scikit-learn
-import pandas as pd
+import pandas as pd        
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
